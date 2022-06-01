3f608a59ba37e2550cc1f62283171f214c9725a1
Danny Linhardt <daniel.linhardt@gmail.com>
2022-06-01 11:45:38 -0400

    got joke and punchline to hide until buttons are clicked

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 // Get data from API
+function fetchJokes() {
+    fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&amount=10")
+    .then(response => response.json())
+    .then(data => deliverJoke(data))
+}
+document.addEventListener("DOMContentLoaded", () => fetchJokes())
 
-// When clicking on the "free joke" button, the tag with id "joke" will be populated and the "gimme the punchline" button will appear
+// When clicking on the "free joke" button, the tag with id "joke-section" will be populated and the "gimme the punchline" button will appear
+
+const freeJoke = document.querySelector("#jokeButton")
+
+function deliverJoke (data) {
+    const jokes = data.jokes
+    jokes.forEach(joke => {
+        const jokeSetUp = joke.setup
+        document.querySelector("#setup").textContent = jokeSetUp
+        const jokePunchline = joke.delivery
+        document.querySelector("#punchline").textContent = jokePunchline
+        document
+        freeJoke.addEventListener("click", (e) => {
+            e.preventDefault()
+            const jokeSection = document.querySelector("#joke-section")
+            jokeSection.removeAttribute("style")
+        })
+        const gimmeThePunchline = document.querySelector("#punch-me")
+        gimmeThePunchline.addEventListener("click", (e) => {
+            e.preventDefault()
+            const punchlineSection = document.querySelector("#punchline-section")
+            punchlineSection.removeAttribute("style")
+})
+    })
+}
 
 // When "good" is input into the text field and submitted, a thumbs up gif will appear. If anything else is typed in, an "oh well" gif will appear.
 
