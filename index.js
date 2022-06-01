9dff9c306138e6a5066fafcc17b6eaebbeab5f4d
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-31 21:48:03 -0400

    added steps to js file and id tag to html

---
+++ index.js
@@ -0,0 +1,7 @@
+// Get data from API
+
+// When clicking on the "free joke" button, the tag with id "joke" will be populated and the "gimme the punchline" button will appear
+
+// When "good" is input into the text field and submitted, a thumbs up gif will appear. If anything else is typed in, an "oh well" gif will appear.
+
+//When the mouse hovers over the image, the page will reset.
